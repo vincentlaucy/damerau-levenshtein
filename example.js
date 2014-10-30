7457d0c1d738e87201fe79967ab2c22778d74f2a
LCY Vincent <lauchunyin@gmail.com>
2014-10-30 15:06:30 +0800

    add shift case

--- example.js
+++ example.js
@@ -1,15 +1,20 @@
 var DL, distance;
-DL = require('damerau-levenshtein');
+DL = require('./damerau-levenshtein');
 
 distance = DL();
 console.log("Default costs (1):");
 console.log("Michael", "Michelle", distance("Michael", "Michelle"));
 
-distance = DL({ insert: 2, remove: 2 });
+distance = DL({
+  insert: 2,
+  remove: 2
+});
 console.log("Insert cost 2 and remove cost 2:");
 console.log("Michael", "Michelle", distance("Michael", "Michelle"));
 
-distance = DL({ transpose: 0.5 });
+distance = DL({
+  transpose: 0.5
+});
 console.log("Transpose cost 0.5:");
 console.log("Michael", "Michelle", distance("Michael", "Michelle"));
 
@@ -22,25 +27,31 @@ console.log("With transposition deactivated:");
 console.log("el", "le", distance("el", "le"));
 
 distance = DL({
-    transpose: function (backward, forward) {
-        if (backward === 'e') return 0.5;
-        else return 1;
-    }
+  transpose: function(backward, forward) {
+    if (backward === 'e') return 0.5;
+    else return 1;
+  }
 });
 console.log("With lower cost of transposing e backwards:");
 console.log("el", "le", distance("el", "le"));
 console.log("le", "el", distance("le", "el"));
 
 distance = DL({
-    insert: 1,
-    remove: 0.5,
-    substitute: 0.8,
-    transpose: function (backward, forward) {
-        if (backward === 'n') return 0.5;
-        else return 1;
-    }
+  insert: 1,
+  remove: 0.5,
+  substitute: 0.8,
+  transpose: function(backward, forward) {
+    if (backward === 'n') return 0.5;
+    else return 1;
+  }
 });
 console.log("A strign", "Another string", distance("A strign", "Another string"));
 
 distance = DL({}, false);
 console.log("A strign", "Another string", distance("A strign", "Another string"));
+
+distance = DL({
+  shift: 0.3
+});
+
+console.log("登入帳號", "帳號申請", distance("登入帳號", "帳號申請"));
